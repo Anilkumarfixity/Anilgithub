715c3613f5fc128b870b93f6f571565551811ac5
AnilkumarFixity <Anilb@fixitytech.com>
2022-12-15 15:06:12 +0530

    commit4

---
+++ ConsoleAppGitHub/Demo/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppGitHub.Demo
+{
+    internal class Class1
+    {
+        public static void test()
+        {
+            Console.WriteLine("Anilkohli");
+        }
+        public static void test2()
+        {
+            Console.WriteLine("Anilkohli");
+        }
+    }
+}
